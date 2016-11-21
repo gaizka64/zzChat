105b366f4ae1638e638cabfd2ac4012936b46294
Ximun <ximun.castoreo@hotmail.fr>
2016-11-21 18:59:18 +0100

    Condensation de quelques lignes de inscription.php.

--- src/inscription.php
+++ src/inscription.php
@@ -9,49 +9,38 @@
 		<h1 class="centrer">ZZ'Chat</h1>
 		<br />
 		<h3 class="centrer">Inscription</h3>
-		<form id="formulaire" action="verif_inscription.php" method="post">
+		<form id="formulaire" action="./verif_inscription.php" method="post">
 			<br />
 			<input type="text" name="login" placeholder="Nom d'utilisateur"> <br>
 			<br />
 			<input type="password" name="pwd" placeholder="Mot de passe"> <br>
 			<br />
-			<input type="password" name="pwd2" placeholder="Confirmer le mot de passe"><br>
+			<input type="password" name="pwd2" placeholder="Confirmer le mot de passe"> <br>
 			<br />
 			<input type="submit" value="Inscription !">
 			<br />
 			<br />
-		 	<?php 	
-
-	 			ini_set('display_errors', true); // Pour activer les messages d'erreur
-
+		 	<?php
 	 			if (isset($_GET["erreur"]) && ($_GET["erreur"] == 1 || $_GET["erreur"] == 2 || $_GET["erreur"] == 3))
 	 			{
 			?>
-
 			<div class="alert alert-danger" role="alert">
 			  	<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-			  	<span class="sr-only">Erreur:</span>
-
+			  	<span class="sr-only">Erreur :</span>
 				<?php
+			 		if ($_GET["erreur"] == 1)
+						echo "Tous les champs doivent être remplis.";
 
-			 		if ($_GET["erreur"] == 1 )
-			 		{
-			 			echo "Tous les champs doivent être remplis.";
-			 		}
 			 		else if ($_GET["erreur"] == 2)
-			 		{
-			 			echo "Les mots de passes saisis ne sont pas identiques.";
-			 		}
-			 		else if ($_GET["erreur"] == 3)
-			 		{
-			 			echo "L'utilisateur existe déjà.";
-			 		}
-			 		?>
-			 		</div>
-			 		<?php
-		 		}?>
-		</form> 
+						echo "Les mots de passes saisis ne sont pas identiques.";
+
+					else if ($_GET["erreur"] == 3)
+						echo "L'utilisateur existe déjà.";
+				?>
+			</div>
+			<?php
+				}
+			?>
+		</form>
 	</body>
 </html>
-
- 
