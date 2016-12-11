4e801c5d8eb817c1f68898c58f9b65390ca6717e
Ximun Castoreo <ximun.castoreo@hotmail.fr>
2016-12-11 19:23:14 +0100

    Mise à jour des tests untaires. Doit régler l'erreur.

--- tests/json_fonctions_test.php
+++ tests/json_fonctions_test.php
@@ -1,22 +1,53 @@
 <?php
+
+require_once 'PHPUnit/Autoload.php';
+require_once('PHPUnit/Framework/TestCase.php');
 use PHPUnit\Framework\TestCase;
-require("json_fonctions.php");
+require_once("../src/json_fonctions.php");
 
 class json_fonctions_test extends TestCase
 {
-    // ...
+	public function testExiste()
+	{
+		$nomFichier = "../db/utilisateurs";
 
-    public function testExiste()
-    {
-        $nomFichier = "utilisateurs";
+		$user  = "testeur_present";
+		$user2 = "testeur_absent";
 
-        $user  = "gaizka";
-        $user2 = "jenofa";
+		// Assertions
+		$this->assertEquals(true, existe($nomFichier,$user));
+		$this->assertEquals(false, existe($nomFichier,$user2));
+	}
 
-        // Assert
-        $this->assertEquals(true, existe($nomFichier,$user));
-        $this->assertEquals(false, existe($nomFichier,$user2));
-    }
+	public function testAjouter()
+	{
+		$nomFichier = "../db/utilisateurs";
+		$userAAjouter = "testeur";
 
-    // ...
+		$this->assertEquals(false, existe($nomFichier, $userAAjouter));
+		ajouter($nomFichier, $userAAjouter, "mdpTesteur");
+		$this->assertEquals(true, existe($nomFichier, $userAAjouter));
+		
+	}	
+
+	public function testSupprimer()
+	{
+		$nomFichier = "../db/utilisateurs";
+		$userASupprimer = "testeur";
+
+		ajouter($nomFichier, $userASupprimer, "mdpTesteur");
+		$this->assertEquals(true, existe($nomFichier, $userASupprimer));
+		supprimer($nomFichier, $userASupprimer);
+		$this->assertEquals(false, existe($nomFichier, $userASupprimer));
+	}
+
+	public function testGetMdp()
+	{
+		$nomFichier = "../db/utilisateurs";
+		$user = "testeur";
+		$mdpUser = "mdpTesteur";
+
+		ajouter($nomFichier, $user, $mdpUser);
+		$this->assertEquals(hash("sha512", $mdpUser), getMdp($nomFichier, $user));
+	}
 }
